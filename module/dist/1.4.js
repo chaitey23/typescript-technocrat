74c3833dfbd96823d1a67d093e09e4a63548631c
chaitey50 <chaiteychaitey30@gmail.com>
2026-01-09 19:28:48 +0600

    learn: add basic TypeScript data types and arrays

---
+++ module/dist/1.4.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// data type
+// string
+let myName = "chaitey";
+// number
+let roll = 1234;
+// boolean
+let isAdmin = true;
+// undefine
+let x = undefined;
+// null 
+let y = null;
+// array
+let friends = ["chaitey", "mithe"];
+friends.push("jui");
+console.log(friends);
+let eligibleRollList = [2, 4, 88, 93];
+eligibleRollList.pop();
+console.log(eligibleRollList);
+let coordinates = [78, "chaetey"];
+coordinates[1] = "jannaatul";
+console.log(coordinates);
---
+++ module/src/1.4.ts
@@ -0,0 +1,21 @@
+// data type
+// string
+let myName: string = "chaitey";
+// number
+let roll: number = 1234;
+// boolean
+let isAdmin: boolean = true;
+// undefine
+let x: undefined = undefined;
+// null 
+let y: null = null;
+// array
+let friends: string[] = ["chaitey", "mithe"]
+friends.push("jui")
+console.log(friends);
+let eligibleRollList: number[] = [2, 4, 88, 93]
+eligibleRollList.pop()
+console.log(eligibleRollList);
+let coordinates: [number, string] = [78, "chaetey"]
+coordinates[1] = "jannaatul"
+console.log(coordinates);
