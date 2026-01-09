b361aaaf1511963b23a1c46e0ba5a824c2eb77cd
chaitey50 <chaiteychaitey30@gmail.com>
2026-01-09 19:48:33 +0600

    feat: add TypeScript object with readonly and optional properties

---
+++ module/src/1.5.ts
@@ -0,0 +1,13 @@
+// reference type 
+const user: {
+    readonly university: "BUBT"; //type --> literal type
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+    isStudent: boolean
+} = {
+    university: "BUBT",
+    firstName: "jannatul",
+    lastName: "chaitey",
+    isStudent: true
+};
