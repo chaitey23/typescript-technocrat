dbd23762bc088fff44a686b4f5f8c1e111def15a
chaitey50 <chaiteychaitey30@gmail.com>
2026-01-11 16:04:00 +0600

    learn union type

---
+++ module/src/1.10.ts
@@ -0,0 +1,30 @@
+{
+    // union type
+
+
+    type FrontendDeveloper = "fakibazDeveloper" | "juniorDeveloper"
+
+
+    const newDeveloper: FrontendDeveloper = "fakibazDeveloper"
+
+    type User = {
+        name: string;
+        email?: string;
+        gender: "male" | "female";
+        bloodGroup: "O+" | "B+" | "A+"
+    }
+
+    const user1: User = {
+        name: "chatey",
+        gender: "female",
+        bloodGroup: "B+"
+    }
+
+
+
+
+
+
+
+    // 
+}
