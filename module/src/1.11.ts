079af902d13d7df52a3ae081db6d17f6489aa9fe
chaitey50 <chaiteychaitey30@gmail.com>
2026-01-11 16:34:06 +0600

    ternary operator , optional chaining ,nullish coalescing operator

---
+++ module/src/1.11.ts
@@ -0,0 +1,27 @@
+{
+    // ternary operator , optional chaining ,nullish coalescing operator
+
+    const age: number = 18;
+
+    if (age >= 18) {
+        // console.log("adult");
+    } else {
+        // console.log("not adult");
+    }
+
+    const isAdult = age >= 18 ? "adult" : "not adult"
+    // console.log({ isAdult });
+
+    // nullish coalescing operator
+    // null /undefine ---> decision making
+
+    const isAuthenticated = null;
+    const result1 = isAuthenticated ?? "Guest"
+    console.log(result1);
+
+
+
+
+
+
+}
